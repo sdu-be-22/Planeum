770b201566ce544442744ed053774f1bee2f649e
Al-Aziz Askhat <64502594+Enderstyles@users.noreply.github.com>
2022-03-10 22:33:43 +0600

    test

--- Planeum/home/views.py
+++ Planeum/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest, HttpResponse
 # Create your views here.
 def homepage(request):
     return render(request=request, template_name='home.html')
+def yes(request):
+    return HttpRequest("sss")
