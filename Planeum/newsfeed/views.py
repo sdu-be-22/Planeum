409fd4b39b66c943c7af9c086ff33a2f17e44832
medeubekovv17 <91006298+medeubekovv17@users.noreply.github.com>
2022-04-01 01:22:11 +0600

    comment, delete, post-detail

--- Planeum/newsfeed/views.py
+++ Planeum/newsfeed/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
-from .models import Post
+from .models import Post, Comment
 from multiprocessing import context
-from .forms import PostForm
+from .forms import PostForm, CommentForm
+from django.views.generic.edit import UpdateView, DeleteView
 from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
@@ -13,7 +15,7 @@ class PostListView(View):
         context = {'post_list':posts,
                    'form': form,
         }
-        return render(request,'post-list.html', context)
+        return render(request, 'newsfeed/post-list.html', context)
 
     def post(self, request, *args, **kwargs):
         posts = Post.objects.all().order_by('-created_on')
@@ -29,6 +31,48 @@ class PostListView(View):
             'form': form,
         }
 
-        return render(request, 'post-list.html', context)    
+        return render(request, 'newsfeed/post-list.html', context)
+    
+class PostDetailView(View):      
+    def get(self,request,pk, *args, **kwargs):
+        post = Post.objects.get(pk = pk)
+        form = CommentForm()
+        
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
+        
+        context = {
+            'post': post,
+            'form': form,
+            'comments': comments,
+        }
+        
+        return render(request, 'newsfeed/post-detail.html', context)
 
-# Create your views here.
+    def post(self, request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        form = CommentForm(request.POST)
+        
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.author = request.user
+            new_comment.post = post
+            new_comment.save()
+            
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
+            
+        context = {
+            'post': post,
+            'form': form,
+            'comments': comments,
+        }
+
+        return render(request, 'newsfeed/post-detail.html', context)
+    
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    template_name = 'newsfeed/comment-delete.html'
+    
+    def get_success_url(self):
+        pk = self.kwargs['post_pk']
+        return reverse_lazy('post-detail', kwargs={'pk':pk})
